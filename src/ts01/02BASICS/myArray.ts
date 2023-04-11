5cd19e6edf75fd16b6fa0cf68c0a65b0e04af586
AneeshBist <aneeshbist@gmail.com>
2023-04-11 17:33:00 +0530

    array file

---
+++ src/ts01/02BASICS/myArray.ts
@@ -0,0 +1,18 @@
+const superHeros:string[] = []
+//const powerHeros:number[] = []
+const powerHeros: Array<number> = []
+
+type UserHero = {
+    name:string
+    isActive: boolean
+}
+const allUsers:UserHero[]=[]
+
+const MLModels: number[][]=[
+    [255,255,255],[209,213,120]
+]
+
+superHeros.push("Spidy")
+powerHeros.push(10)
+
+allUsers.push({name:"jimmy", isActive:false})
